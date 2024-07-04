df295383ba99a7b663c09e2864fcba7a7867add5
Chloe Hsu <chloehsu@ChloedeMacBook-Pro.local>
2024-07-04 17:02:59 +0800

    feat:1.implement Localization,2.add audit trail for sending request to Coindesk

--- CurrencyApi/Controllers/CurrencyController.cs
+++ CurrencyApi/Controllers/CurrencyController.cs
@@ -2,6 +2,7 @@
 using AutoMapper.QueryableExtensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
 
 namespace CurrencyApi;
 [ApiController]
@@ -10,10 +11,13 @@ public class CurrencyController : ControllerBase
 {
     private readonly CurrencyDBContext _context;
     private readonly IMapper _mapper;
-    public CurrencyController(CurrencyDBContext context, IMapper mapper)
+    private readonly IStringLocalizer<CurrencyController> _localizer;
+
+    public CurrencyController(CurrencyDBContext context, IMapper mapper, IStringLocalizer<CurrencyController> localizer)
     {
         _context = context;
         _mapper = mapper;
+        _localizer = localizer;
     }
     [HttpGet]
     public async Task<ActionResult<List<CurrenciesDto>>> GetAllCurrencies()
@@ -25,13 +29,18 @@ public class CurrencyController : ControllerBase
     [HttpGet("{code}")]
     public async Task<ActionResult<CurrenciesDto>> GetCurrency(string code)
     {
+        if(string.IsNullOrEmpty(code)) return BadRequest(_localizer["Code field is required"].Value);
         var query = await _context.Currencies.Include(x => x.Language).FirstOrDefaultAsync(x => x.Code == code);
-        if (query == null) return NotFound();
+        if (query == null) return BadRequest(_localizer["Invalid Code"].Value);
         return _mapper.Map<CurrenciesDto>(query);
     }
     [HttpPost]
     public async Task<ActionResult<CurrenciesDto>> CreateCurrency(CreateCurrencyDto newCurrency)
     {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
         var currency = _mapper.Map<Currency>(newCurrency);
         currency.UpdatedUTC = DateTime.UtcNow;
         currency.Language.Add(new Translation()
@@ -42,7 +51,7 @@ public class CurrencyController : ControllerBase
         _context.Currencies.Add(currency);
 
         var result = await _context.SaveChangesAsync() > 0;
-        if (!result) return BadRequest("Could not save data to DB");
+        if (!result) return BadRequest(_localizer["Database update failed"].Value);
 
         return CreatedAtAction(nameof(GetCurrency), new { currency.Code }, _mapper.Map<CurrenciesDto>(currency));
     }
@@ -50,8 +59,13 @@ public class CurrencyController : ControllerBase
     [HttpPut("{code}")]
     public async Task<ActionResult<CurrenciesDto>> UpdateCurrency(string code, UpdateCurrencyDto updateCurrency)
     {
+        if(string.IsNullOrEmpty(code.Trim())) return BadRequest(_localizer["Code field is required"].Value);
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
         var currency = await _context.Currencies.Include(x => x.Language).FirstOrDefaultAsync(x => x.Code == code);
-        if (currency == null) return NotFound();
+        if (currency == null) return BadRequest(_localizer["Invalid Code"].Value);
 
         currency.Name = String.IsNullOrEmpty(updateCurrency.Name) ? currency.Name : updateCurrency.Name;
         currency.Rate = updateCurrency.Rate == 0 ? currency.Rate : updateCurrency.Rate;
@@ -68,17 +82,17 @@ public class CurrencyController : ControllerBase
 
         var result = await _context.SaveChangesAsync() > 0;
         if (result) return Ok();
-        return BadRequest("Currency update failed");
+        return BadRequest(_localizer["Database update failed"].Value);
     }
     [HttpDelete("{code}")]
     public async Task<ActionResult> DeleteCurrency(string code)
     {
+        if(string.IsNullOrEmpty(code)) return BadRequest(_localizer["Code field is required"].Value);
         var currency = await _context.Currencies.FirstOrDefaultAsync(x => x.Code == code);
-        if (currency == null) return NotFound();
+        if (currency == null) return BadRequest(_localizer["Invalid Code"].Value);
         _context.Currencies.Remove(currency);
         var result = await _context.SaveChangesAsync() > 0;
-        if (!result) return BadRequest("Could not update DB");
+        if (!result) return BadRequest(_localizer["Database update failed"].Value);
         return Ok();
     }
-
 }
--- CurrencyApi/Services/CoindeskApiHttpClient.cs
+++ CurrencyApi/Services/CoindeskApiHttpClient.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Newtonsoft.Json;
 
 namespace CurrencyApi;
 
@@ -6,22 +7,35 @@ public class CoindeskApiHttpClient
 {
     private readonly HttpClient _httpClient;
     private readonly IConfiguration _config;
+    private readonly CurrencyDBContext _context;
 
-    public CoindeskApiHttpClient(HttpClient httpClient,IConfiguration config)
+    public CoindeskApiHttpClient(HttpClient httpClient, IConfiguration config, CurrencyDBContext context)
     {
         _httpClient = httpClient;
         _config = config;
+        _context = context;
     }
 
     public async Task<List<Currency>> GetTransformedCoindeskData()
     {
-        var response = await _httpClient.GetFromJsonAsync<CoindeskResponseDto>(_config["CoindeskAPI"]);
-        var item =response.bpi.Select(x => new Currency(){
+        var audit = new AuditTrail();
+
+        audit.ReqeustDateUTC = DateTime.UtcNow;
+        audit.ClientIP = _httpClient.BaseAddress?.ToString();
+        audit.Controller = _config["CoindeskAPI"];
+
+        var response = await _httpClient.GetFromJsonAsync<CoindeskResponseDto>(audit.Controller);
+        audit.ResponseDateUTC = DateTime.UtcNow;
+        audit.Response = JsonConvert.SerializeObject(response);
+        audit.HttpStatus = 200;
+        _context.AuditTrail.Add(audit);
+        await _context.SaveChangesAsync();
+        return response.bpi.Select(x => new Currency()
+        {
             Code = x.Value.code,
             Name = x.Value.description,
             Rate = x.Value.rate_float,
             UpdatedUTC = response.time.updatedISO.ToUniversalTime(),
-        }).ToList();
-        return item;
+        }).ToList(); ;
     }
 }
